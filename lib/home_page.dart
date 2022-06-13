3b37ea3ccc0a763e60ab5de5334ee1c01f0acdc1
MahdiGharooni <39171814+MahdiGharooni@users.noreply.github.com>
2022-06-13 10:27:19 +0430

    imp home page

---
+++ lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:xml_parser/browse_page/browse_page.dart';
+import 'package:xml_parser/sample_page/sample_page.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('XML PARSER'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) => const SamplePage()));
+              },
+              child: const Text('Sample'),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) => const BrowsePage()));
+              },
+              child: const Text('Browse'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
